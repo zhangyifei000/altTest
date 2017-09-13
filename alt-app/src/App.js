78e6bd0cc6212d50d5835a8ca52bce41505a3e86
Bill Zhang <bill.zhang@otms.cn>
2017-09-13 14:45:32 +0800

    add ALtContainer transform render

--- alt-app/src/App.js
+++ alt-app/src/App.js
@@ -8,7 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AltContainer store={LocationStore}>
+        <AltContainer store={LocationStore} transform={(locationStore) => {
+            const {locations}=locationStore;
+            locations.push({id: 100, name: "上海"});
+
+            return {locations}
+          }} /*render={(locationStore) => {
+            const {locations} = locationStore
+            const name = locations[0].name
+            //进行render
+            return <i>{name}</i>
+          }}*/>
           <LocationContainer />
         </AltContainer>
       </div>
